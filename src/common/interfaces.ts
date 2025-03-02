1fef5ddbfd529cbf779e42efff7c4f00e95f7079
Niv Greenstein <greenstein9@gmail.com>
2025-03-02 16:52:51 +0200

    refactor: removed interfaces as no longer needed

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -1,41 +1,5 @@
-import { Source } from '../tiles/models/tiles';
+import { type vectorMetatileQueuePopulatorV1Type } from '@map-colonies/schemas';
 
-export interface IConfig {
-  get: <T>(setting: string) => T;
-  has: (setting: string) => boolean;
-}
-
-export interface OpenApiConfig {
-  filePath: string;
-  basePath: string;
-  jsonPath: string;
-  uiPath: string;
-}
-
-export interface QueueConfig {
-  expireInSeconds: number;
-  retryBackoff: boolean;
-  retryLimit: number;
-  retryDelaySeconds: number;
-  retentionHours: number;
-}
-
-export interface JobInsertConfig extends Partial<Omit<QueueConfig, 'retryDelaySeconds'>> {
+export interface JobInsertConfig extends Partial<Omit<vectorMetatileQueuePopulatorV1Type['queue'], 'retryDelaySeconds'>> {
   retryDelay?: number;
-}
-
-export interface AppConfig {
-  projectName: string;
-  enableRequestQueueHandling: boolean;
-  requestQueueCheckIntervalSec: number;
-  tilesBatchSize: number;
-  metatileSize: number;
-  consumeDelay: {
-    enabled: boolean;
-    delaySec: number;
-    tilesQueueSizeLimit: number;
-  };
-  force?: {
-    [key in Source]?: boolean;
-  };
 }
