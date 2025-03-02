618ffc3d33d651c8d7554db002f5bf4a2792aded
Niv Greenstein <greenstein9@gmail.com>
2025-03-02 16:54:54 +0200

    refactor: updated getApp function

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,27 @@
-/* eslint-disable import/first */
-// this import must be called before the first import of tsyring
+// this import must be called before the first import of tsyringe
 import 'reflect-metadata';
-import './common/tracing';
 import { createServer } from 'http';
-import { createTerminus, HealthCheck } from '@godaddy/terminus';
+import { createTerminus } from '@godaddy/terminus';
 import { Logger } from '@map-colonies/js-logger';
-import config from 'config';
-import { DependencyContainer } from 'tsyringe';
-import { CONSUME_AND_POPULATE_FACTORY, DEFAULT_SERVER_PORT, HEALTHCHECK_SYMBOL, ON_SIGNAL, SERVICES } from './common/constants';
+import { ON_SIGNAL, SERVICES } from '@common/constants';
+import { ConfigType } from '@common/config';
 import { getApp } from './app';
-import { IConfig } from './common/interfaces';
-
-let depContainer: DependencyContainer | undefined;
-
-const port: number = config.get<number>('server.port') || DEFAULT_SERVER_PORT;
 
 void getApp()
-  .then(async ([app, container]) => {
-    depContainer = container;
-
-    const logger = depContainer.resolve<Logger>(SERVICES.LOGGER);
-    const config = depContainer.resolve<IConfig>(SERVICES.CONFIG);
-    const healthCheck = depContainer.resolve<HealthCheck | boolean>(HEALTHCHECK_SYMBOL);
-
-    const server = createTerminus(createServer(app), {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      healthChecks: { '/liveness': healthCheck, '/readiness': healthCheck },
-      onSignal: depContainer.resolve(ON_SIGNAL),
-    });
+  .then(([app, container]) => {
+    const logger = container.resolve<Logger>(SERVICES.LOGGER);
+    const config = container.resolve<ConfigType>(SERVICES.CONFIG);
+    const port = config.get('server.port');
+    const stubHealthCheck = async (): Promise<void> => Promise.resolve();
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const server = createTerminus(createServer(app), { healthChecks: { '/liveness': stubHealthCheck }, onSignal: container.resolve(ON_SIGNAL) });
 
     server.listen(port, () => {
       logger.info(`app started on port ${port}`);
     });
-
-    if (config.get<boolean>('app.enableRequestQueueHandling')) {
-      const consumeAndPopulate = container.resolve<() => Promise<void>>(CONSUME_AND_POPULATE_FACTORY);
-      await consumeAndPopulate();
-    }
   })
-  .catch(async (error: Error) => {
-    const errorLogger =
-      depContainer?.isRegistered(SERVICES.LOGGER) === true
-        ? depContainer.resolve<Logger>(SERVICES.LOGGER).error.bind(depContainer.resolve<Logger>(SERVICES.LOGGER))
-        : console.error;
-    errorLogger({ msg: '😢 - failed initializing the server', err: error });
-
-    if (depContainer?.isRegistered(ON_SIGNAL) === true) {
-      const shutDown: () => Promise<void> = depContainer.resolve(ON_SIGNAL);
-      await shutDown();
-    }
+  .catch((error: Error) => {
+    console.error('😢 - failed initializing the server');
+    console.error(error);
+    process.exit(1);
   });
