d8bea82f147139b7c8d5646adfbd02a6ae467275
Niv Greenstein <greenstein9@gmail.com>
2025-03-02 16:52:17 +0200

    refactor: update code style

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,25 +1,25 @@
 import { readPackageJsonSync } from '@map-colonies/read-pkg';
 
 export const SERVICE_NAME = readPackageJsonSync().name ?? 'unknown_service';
-export const DEFAULT_SERVER_PORT = 8080;
+export const DEFAULT_SERVER_PORT = 80;
 
 export const IGNORED_OUTGOING_TRACE_ROUTES = [/^.*\/v1\/metrics.*$/];
-export const IGNORED_INCOMING_TRACE_ROUTES = [/^.*\/docs.*$/, /^.*\/metrics.*/];
-
-export const HEALTHCHECK_SYMBOL = Symbol('healthcheck');
-export const ON_SIGNAL = Symbol('onSignal');
-export const CONSUME_AND_POPULATE_FACTORY = Symbol('consumeAndPopulateFactory');
-export const JOB_QUEUE_PROVIDER = Symbol('JobQueueProvider');
+export const IGNORED_INCOMING_TRACE_ROUTES = [/^.*\/docs.*$/];
 
 /* eslint-disable @typescript-eslint/naming-convention */
-export const SERVICES: Record<string, symbol> = {
+export const SERVICES = {
   LOGGER: Symbol('Logger'),
   CONFIG: Symbol('Config'),
   TRACER: Symbol('Tracer'),
-  METER: Symbol('Meter'),
-  METRICS_REGISTRY: Symbol('MetricsRegistry'),
+  METRICS: Symbol('Metrics'),
   CLEANUP_REGISTRY: Symbol('CleanupRegistry'),
-};
+} satisfies Record<string, symbol>;
 /* eslint-enable @typescript-eslint/naming-convention */
+
+export const HEALTHCHECK = Symbol('healthcheck');
+export const ON_SIGNAL = Symbol('onSignal');
+
+export const CONSUME_AND_POPULATE_FACTORY = Symbol('consumeAndPopulateFactory');
+export const JOB_QUEUE_PROVIDER = Symbol('JobQueueProvider');
 
 export const MILLISECONDS_IN_SECOND = 1000;
