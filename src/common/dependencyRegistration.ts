257066a00b9c26167186b0f74ad1438c1655caf6
Niv Greenstein <greenstein9@gmail.com>
2025-03-02 16:52:33 +0200

    feat: added postInjectionHook, afterAllInjectionHook

--- src/common/dependencyRegistration.ts
+++ src/common/dependencyRegistration.ts
@@ -1,5 +1,5 @@
-import { ClassProvider, container as defaultContainer, FactoryProvider, InjectionToken, ValueProvider } from 'tsyringe';
-import { constructor, DependencyContainer, RegistrationOptions } from 'tsyringe/dist/typings/types';
+import { ClassProvider, container as defaultContainer, FactoryProvider, InjectionToken, ValueProvider, RegistrationOptions } from 'tsyringe';
+import { constructor, DependencyContainer } from 'tsyringe/dist/typings/types';
 
 export type Providers<T> = ValueProvider<T> | FactoryProvider<T> | ClassProvider<T> | constructor<T>;
 
@@ -7,22 +7,25 @@ export interface InjectionObject<T> {
   token: InjectionToken<T>;
   provider: Providers<T>;
   options?: RegistrationOptions;
+  postInjectionHook?: (container: DependencyContainer) => void | Promise<void>;
+  afterAllInjectionHook?: (container: DependencyContainer) => void | Promise<void>;
 }
 
-export const registerDependencies = (
+export const registerDependencies = async (
   dependencies: InjectionObject<unknown>[],
   override?: InjectionObject<unknown>[],
   useChild = false
-): DependencyContainer => {
+): Promise<DependencyContainer> => {
   const container = useChild ? defaultContainer.createChildContainer() : defaultContainer;
-  dependencies.forEach((injectionObj) => {
-    const inject = override?.find((overrideObj) => overrideObj.token === injectionObj.token) === undefined;
-    if (inject) {
-      container.register(injectionObj.token, injectionObj.provider as constructor<unknown>, injectionObj.options);
-    }
-  });
-  override?.forEach((injectionObj) => {
+  for (const dep of dependencies) {
+    const injectionObj = override?.find((overrideObj) => overrideObj.token === dep.token) ?? dep;
     container.register(injectionObj.token, injectionObj.provider as constructor<unknown>, injectionObj.options);
-  });
+    await dep.postInjectionHook?.(container);
+  }
+
+  for (const dep of dependencies) {
+    await dep.afterAllInjectionHook?.(container);
+  }
+
   return container;
 };
