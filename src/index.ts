12e1b075d7058d008fcf3812d4fb31cd34239ad8
Niv Greenstein <greenstein9@gmail.com>
2025-03-12 16:33:42 +0200

    fix: added missing shutdown function

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 // this import must be called before the first import of tsyringe
 import 'reflect-metadata';
 import { createServer } from 'http';
+import { DependencyContainer } from 'tsyringe';
 import { createTerminus } from '@godaddy/terminus';
 import { Logger } from '@map-colonies/js-logger';
 import { ON_SIGNAL, SERVICES } from '@common/constants';
 import { ConfigType } from '@common/config';
 import { getApp } from './app';
+
+let container: DependencyContainer | undefined;
 
 void getApp()
   .then(([app, container]) => {
@@ -20,8 +23,14 @@ void getApp()
       logger.info(`app started on port ${port}`);
     });
   })
-  .catch((error: Error) => {
+  .catch(async (error: Error) => {
     console.error('😢 - failed initializing the server');
     console.error(error);
+
+    if (container?.isRegistered(ON_SIGNAL) === true) {
+      const shutdown = container.resolve<() => Promise<void>>(ON_SIGNAL);
+      await shutdown();
+    }
+
     process.exit(1);
   });
