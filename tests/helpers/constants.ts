7d583206a4e30f987b2977013c922c5fdfba40f5
Niv Greenstein <88280771+NivGreenstein@users.noreply.github.com>
2026-02-08 17:54:05 +0200

    fix: added queue to queueNames

--- tests/helpers/constants.ts
+++ tests/helpers/constants.ts
@@ -1,6 +1,7 @@
 import { QueueNames } from '@src/tiles/jobQueueProvider/queuesNameFactory';
 
-export const queueNames: QueueNames = {
+export const queueNames: QueueNames & { [x: string]: string } = {
+  requestTestQueue: 'tiles-requests-test-requests',
   requestQueue: 'tiles-requests-test',
   tilesQueue: 'tiles-test',
 };
